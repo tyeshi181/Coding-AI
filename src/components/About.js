7c66ec8effd3654b39a2827da1af0e2d613f023a
Yeshi <tyeshi777@yahoo.com>
2019-10-08 09:03:57 -0700

    added some contents to the home page with the react logo. same contents on about page. made the links functioning so that you can jump from page to page

--- src/components/About.js
+++ src/components/About.js
@@ -4,9 +4,20 @@ import React from "react";
 const about = () => {
   return (
     <div title="About">
-      <h1>About Page</h1>
+      <h1 className="aboutH1">About Page</h1>
+      <p className="about">
+        Whether you're just starting to learn to program for the first time or
+        you've been working as a developer for years, having a mentor is one of
+        the most effective ways to continue improving and ensure you're
+        constantly expanding your skill-set. Studies show that learning
+        one-on-one with a teacher can drastically impact your performance and
+        ability to improve. Coding-AI will try to match you with a mentor
+        because of how difficult it was to find mentors when learning to code.
+        With so many people entering the engineering world, mentorship is in
+        high demand, and time is a valuable resource for most successful coders.
+      </p>
     </div>
   );
-}
+};
 
 export default about;
