541d101a614ffedd13a24c6761936eb73e04d4c5
Yeshi <tyeshi777@yahoo.com>
2019-10-07 19:15:52 -0700

    added mentors data iwith required fields

--- src/response/response.js
+++ src/response/response.js
@@ -1,88 +1,95 @@
 const data = [
-    {
-        name: 'Elangovan',
-        image: 'https://via.placeholder.com/300/',
-        technology: 'ReactJs, Javascript',
-        country: 'India',
-        biography: 'I am Full Stack Web Developer'
-    },
-    {
-        name: 'Krish',
-        image: 'https://via.placeholder.com/300/',
-        technology: 'Java, ReactJs, Javascript',
-        country: 'United States',
-        biography: 'I am Full Stack Web Developer'
-    },
-    {
-        name: 'John',
-        image: 'https://via.placeholder.com/300/',
-        technology: 'Php, Javascript',
-        country: 'India',
-        biography: 'I am Full Stack Web Developer'
-    },
-    {
-        name: 'Naresh',
-        image: 'https://via.placeholder.com/300/',
-        technology: 'AngularJS, Javascript',
-        country: 'India',
-        biography: 'I am Full Stack Web Developer'
-    },
-    {
-        name: 'Abdul',
-        image: 'https://via.placeholder.com/300/',
-        technology: 'Angularjs, Python',
-        country: 'India',
-        biography: 'I am Full Stack Web Developer'
-    },
-    {
-        name: 'Elangovan',
-        image: 'https://via.placeholder.com/300/',
-        technology: 'ReactJs, Javascript',
-        country: 'India',
-        biography: 'I am Full Stack Web Developer'
-    },
-    {
-        name: 'Krish',
-        image: 'https://via.placeholder.com/300/',
-        technology: 'Java, ReactJs, Javascript',
-        country: 'United States',
-        biography: 'I am Full Stack Web Developer'
-    },
-    {
-        name: 'John',
-        image: 'https://via.placeholder.com/300/',
-        technology: 'Php, Javascript',
-        country: 'India',
-        biography: 'I am Full Stack Web Developer'
-    },
-    {
-        name: 'Naresh',
-        image: 'https://via.placeholder.com/300/',
-        technology: 'AngularJS, Javascript',
-        country: 'India',
-        biography: 'I am Full Stack Web Developer'
-    },
-    {
-        name: 'Abdul',
-        image: 'https://via.placeholder.com/300/',
-        technology: 'Angularjs, Python',
-        country: 'India',
-        biography: 'I am Full Stack Web Developer'
-    },
-    {
-        name: 'Krish',
-        image: 'https://via.placeholder.com/300/',
-        technology: 'Java, ReactJs, Javascript',
-        country: 'United States',
-        biography: 'I am Full Stack Web Developer'
-    },
-    {
-        name: 'John',
-        image: 'https://via.placeholder.com/300/',
-        technology: 'Php, Javascript',
-        country: 'India',
-        biography: 'I am Full Stack Web Developer'
-    }
+  {
+    name: "Elangovan",
+    image: "https://via.placeholder.com/300/",
+    technology: "ReactJs, Javascript",
+    country: "India",
+    biography: "I am Full Stack Web Developer"
+  },
+  {
+    name: "Krish",
+    image: "https://via.placeholder.com/300/",
+    technology: "Java, ReactJs, Javascript",
+    country: "United States",
+    biography: "I am Full Stack Web Developer"
+  },
+  {
+    name: "John",
+    image: "https://via.placeholder.com/300/",
+    technology: "Php, Javascript",
+    country: "India",
+    biography: "I am Full Stack Web Developer"
+  },
+  {
+    name: "Naresh",
+    image: "https://via.placeholder.com/300/",
+    technology: "AngularJS, Javascript",
+    country: "India",
+    biography: "I am Full Stack Web Developer"
+  },
+  {
+    name: "Abdul",
+    image: "https://via.placeholder.com/300/",
+    technology: "Angularjs, Python",
+    country: "India",
+    biography: "I am Full Stack Web Developer"
+  },
+  {
+    name: "Elangovan",
+    image: "https://via.placeholder.com/300/",
+    technology: "ReactJs, Javascript",
+    country: "India",
+    biography: "I am Full Stack Web Developer"
+  },
+  {
+    name: "Krish",
+    image: "https://via.placeholder.com/300/",
+    technology: "Java, ReactJs, Javascript",
+    country: "United States",
+    biography: "I am Full Stack Web Developer"
+  },
+  {
+    name: "John",
+    image: "https://via.placeholder.com/300/",
+    technology: "Php, Javascript",
+    country: "India",
+    biography: "I am Full Stack Web Developer"
+  },
+  {
+    name: "Naresh",
+    image: "https://via.placeholder.com/300/",
+    technology: "AngularJS, Javascript",
+    country: "India",
+    biography: "I am Full Stack Web Developer"
+  },
+  {
+    name: "Abdul",
+    image: "https://via.placeholder.com/300/",
+    technology: "Angularjs, Python",
+    country: "India",
+    biography: "I am Full Stack Web Developer"
+  },
+  {
+    name: "Krish",
+    image: "https://via.placeholder.com/300/",
+    technology: "Java, ReactJs, Javascript",
+    country: "United States",
+    biography: "I am Full Stack Web Developer"
+  },
+  {
+    name: "John",
+    image: "https://via.placeholder.com/300/",
+    technology: "Php, Javascript",
+    country: "India",
+    biography: "I am Full Stack Web Developer"
+  },
+  {
+    name: "Tenzing",
+    image: "https://via.placeholder.com/300/",
+    technology: "Python, Javascript",
+    country: "USA",
+    biography: "I am Full Stack Web Developer"
+  }
 ];
 
 export default data;
